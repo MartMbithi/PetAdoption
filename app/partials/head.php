4c3f6c25225963599303bf0814a6fe0730f17b11
Martin Mbithi <martdevelopers254@gmail.com>
2022-07-23 10:01:21 +0300

    Implement Login UI

---
+++ app/partials/head.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en-US" dir="ltr">
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- ===============================================-->
+    <!--    Document Title-->
+    <!-- ===============================================-->
+    <title>Pet Adoption System</title>
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- ===============================================-->
+    <!--    Stylesheets-->
+    <!-- ===============================================-->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700%7cPoppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="../assets/app_css/theme.css" rel="stylesheet">
+</head>
---
+++ app/partials/scripts.php
@@ -0,0 +1,7 @@
+<script src="../assets/app_js/jquery.min.js"></script>
+<script src="../assets/app_js/popper.min.js"></script>
+<script src="../assets/app_js/bootstrap.js"></script>
+<script src="../assets/app_js/plugins.js"></script>
+<script src="../assets/plugins/stickyfilljs/stickyfill.min.js"></script>
+<script src="../assets/plugins/sticky-kit/sticky-kit.min.js"></script>
+<script src="../assets/app_js/theme.js"></script>
