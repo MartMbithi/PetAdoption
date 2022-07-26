fa82663610f94e33be18897688473b3d8873568d
Martin Mbithi <martdevelopers254@gmail.com>
2022-07-26 18:09:23 +0300

    Gave Application Scope

--- app/partials/footer.php
+++ app/partials/footer.php
@@ -5,3 +5,26 @@
             <b>Version</b> 1.0
         </div>
 </footer>
+
+<!-- End Session Modal -->
+<div class="modal fade" id="logout_modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">CONFIRM SESSION TERMINATION</h5>
+                <button type="button" class="close" data-dismiss="modal">
+                    <span>&times;</span>
+                </button>
+            </div>
+            <div class="modal-body text-center ">
+                <h4 class="text-danger">
+                    Ready To Terminate Current Session And Logout ?
+                </h4>
+                <br>
+                <button type="button" class="text-center btn btn-success" data-dismiss="modal"> No</button>
+                <a class="text-center btn btn-danger" href="logout"> Yes, Log Out</a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Modal -->
