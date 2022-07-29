71ac759c35a5d1a7be12a2db002ab6857f202e1e
Martin Mbithi <martdevelopers254@gmail.com>
2022-07-29 13:50:28 +0300

    Implement Pet Owners Front End Page - Create, Fetch , Update And Delete

---
+++ ui/pet_owners.php
@@ -0,0 +1,275 @@
+<?php
+/*
+ *   Crafted On Tue Jul 26 2022
+ *
+ * 
+ *   https://bit.ly/MartMbithi
+ *   martdevelopers254@gmail.com
+ *
+ *
+ *   The MartDevelopers End User License Agreement
+ *   Copyright (c) 2022 MartDevelopers
+ *
+ *
+ *   1. GRANT OF LICENSE 
+ *   MartDevelopers hereby grants to you (an individual) the revocable, personal, non-exclusive, and nontransferable right to
+ *   install and activate this system on two separated computers solely for your personal and non-commercial use,
+ *   unless you have purchased a commercial license from MartDevelopers. Sharing this Software with other individuals, 
+ *   or allowing other individuals to view the contents of this Software, is in violation of this license.
+ *   You may not make the Software available on a network, or in any way provide the Software to multiple users
+ *   unless you have first purchased at least a multi-user license from MartDevelopers.
+ *
+ *   2. COPYRIGHT 
+ *   The Software is owned by MartDevelopers and protected by copyright law and international copyright treaties. 
+ *   You may not remove or conceal any proprietary notices, labels or marks from the Software.
+ *
+ *
+ *   3. RESTRICTIONS ON USE
+ *   You may not, and you may not permit others to
+ *   (a) reverse engineer, decompile, decode, decrypt, disassemble, or in any way derive source code from, the Software;
+ *   (b) modify, distribute, or create derivative works of the Software;
+ *   (c) copy (other than one back-up copy), distribute, publicly display, transmit, sell, rent, lease or 
+ *   otherwise exploit the Software. 
+ *
+ *
+ *   4. TERM
+ *   This License is effective until terminated. 
+ *   You may terminate it at any time by destroying the Software, together with all copies thereof.
+ *   This License will also terminate if you fail to comply with any term or condition of this Agreement.
+ *   Upon such termination, you agree to destroy the Software, together with all copies thereof.
+ *
+ *
+ *   5. NO OTHER WARRANTIES. 
+ *   MARTDEVELOPERS  DOES NOT WARRANT THAT THE SOFTWARE IS ERROR FREE. 
+ *   MARTDEVELOPERS SOFTWARE DISCLAIMS ALL OTHER WARRANTIES WITH RESPECT TO THE SOFTWARE, 
+ *   EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO IMPLIED WARRANTIES OF MERCHANTABILITY, 
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF THIRD PARTY RIGHTS. 
+ *   SOME JURISDICTIONS DO NOT ALLOW THE EXCLUSION OF IMPLIED WARRANTIES OR LIMITATIONS
+ *   ON HOW LONG AN IMPLIED WARRANTY MAY LAST, OR THE EXCLUSION OR LIMITATION OF 
+ *   INCIDENTAL OR CONSEQUENTIAL DAMAGES,
+ *   SO THE ABOVE LIMITATIONS OR EXCLUSIONS MAY NOT APPLY TO YOU. 
+ *   THIS WARRANTY GIVES YOU SPECIFIC LEGAL RIGHTS AND YOU MAY ALSO 
+ *   HAVE OTHER RIGHTS WHICH VARY FROM JURISDICTION TO JURISDICTION.
+ *
+ *
+ *   6. SEVERABILITY
+ *   In the event of invalidity of any provision of this license, the parties agree that such invalidity shall not
+ *   affect the validity of the remaining portions of this license.
+ *
+ *
+ *   7. NO LIABILITY FOR CONSEQUENTIAL DAMAGES IN NO EVENT SHALL MARTDEVELOPERS OR ITS SUPPLIERS BE LIABLE TO YOU FOR ANY
+ *   CONSEQUENTIAL, SPECIAL, INCIDENTAL OR INDIRECT DAMAGES OF ANY KIND ARISING OUT OF THE DELIVERY, PERFORMANCE OR 
+ *   USE OF THE SOFTWARE, EVEN IF MARTDEVELOPERS HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES
+ *   IN NO EVENT WILL MARTDEVELOPERS  LIABILITY FOR ANY CLAIM, WHETHER IN CONTRACT 
+ *   TORT OR ANY OTHER THEORY OF LIABILITY, EXCEED THE LICENSE FEE PAID BY YOU, IF ANY.
+ *
+ */
+session_start();
+require_once('../app/settings/config.php');
+require_once('../app/settings/codeGen.php');
+require_once('../app/settings/checklogin.php');
+check_login();
+require_once('../app/helpers/users.php');
+require_once('../app/partials/head.php');
+?>
+
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+    <div class="wrapper">
+        <!-- Navbar -->
+        <?php require_once('../app/partials/navbar.php'); ?>
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <?php require_once('../app/partials/aside.php'); ?>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0 text-dark">Pet Owners</h1>
+                        </div><!-- /.col -->
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="dashboard">Dashboard</a></li>
+                                <li class="breadcrumb-item active">Pet Owners</li>
+                            </ol>
+                        </div><!-- /.col -->
+                        <hr>
+                    </div><!-- /.row -->
+                </div><!-- /.container-fluid -->
+                <div class="card-header p-2">
+                    <h3 class="text-right">
+                        <button type="button" data-toggle="modal" data-target="#add_modal" class="btn btn-outline-primary">
+                            <i class="fas fa-user-plus"></i> Register New Pet Owner
+                        </button>
+                    </h3>
+                </div><!-- /.card-header -->
+            </div>
+            <!-- /.content-header -->
+
+            <div class="modal fade" id="add_modal">
+                <div class="modal-dialog modal-dialog-centered  modal-lg">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="modal-title">Register New Pet Owner - Fill All Required Fields </h4>
+                            <button type="button" class="close" data-dismiss="modal">
+                                <span>&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <form method="POST">
+                                <div class="form-row">
+                                    <div class="form-group col-12">
+                                        <label>Full Names</label>
+                                        <div class="input-group mb-3">
+                                            <input class="form-control" required type="text" name="adopter_full_name">
+                                            <div class="input-group-append">
+                                                <div class="input-group-text">
+                                                    <span class="text-primary fas fa-user-tag"></span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group col-6">
+                                        <label>Contacts</label>
+                                        <div class="input-group mb-3">
+                                            <input class="form-control" required type="text" name="adoper_contacts">
+                                            <div class="input-group-append">
+                                                <div class="input-group-text">
+                                                    <span class="text-primary fas fa-phone"></span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group col-6">
+                                        <label>Email Address</label>
+                                        <div class="input-group mb-3">
+                                            <input class="form-control" required type="text" name="adopter_email">
+                                            <div class="input-group-append">
+                                                <div class="input-group-text">
+                                                    <span class="text-primary fas fa-envelope"></span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group col-12">
+                                        <label>Address</label>
+                                        <div class="input-group mb-3">
+                                            <input class="form-control" required type="text" name="adopter_location">
+                                            <div class="input-group-append">
+                                                <div class="input-group-text">
+                                                    <span class="text-primary fas fa-map-marker-alt"></span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group col-6">
+                                        <label>Login Password</label>
+                                        <div class="input-group mb-3">
+                                            <input class="form-control" required type="password" name="new_password">
+                                            <div class="input-group-append">
+                                                <div class="input-group-text">
+                                                    <span class="text-primary fas fa-user-lock"></span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group col-6">
+                                        <label>Confirm Password</label>
+                                        <div class="input-group mb-3">
+                                            <input class="form-control" required type="password" name="confirm_password">
+                                            <div class="input-group-append">
+                                                <div class="input-group-text">
+                                                    <span class="text-primary fas fa-user-lock"></span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group text-right">
+                                    <button class="btn btn-outline-primary mt-3 " type="submit" name="Register_Pet_Adopter" name="submit">
+                                        <i class="fas fa-save"></i> Save
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card card-primary card-outline">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <table class="table table-bordered text-truncate" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                                                <thead>
+                                                    <tr>
+                                                        <th>Full Names</th>
+                                                        <th>Contacts</th>
+                                                        <th>Email</th>
+                                                        <th>Address</th>
+                                                        <th>Manage</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <?php
+                                                    $ret = "SELECT * FROM pet_owner";
+                                                    $stmt = $mysqli->prepare($ret);
+                                                    $stmt->execute(); //ok
+                                                    $res = $stmt->get_result();
+                                                    while ($owners = $res->fetch_object()) {
+                                                    ?>
+                                                        <tr>
+                                                            <td>
+                                                                <?php echo $owners->pet_owner_full_name; ?>
+                                                            </td>
+                                                            <td>
+                                                                <?php echo $owners->pet_owner_contacts; ?>
+                                                            </td>
+                                                            <td><?php echo $owners->pet_owner_email; ?></td>
+                                                            <td><?php echo $owners->pet_owner_email; ?></td>
+                                                            <td>
+                                                                <a data-toggle="modal" href="#update_<?php echo $owners->pet_owner_id; ?>" class="badge  badge-pill badge-warning"><em class="fas fa-user-edit"></em> Edit</a>
+                                                                <a data-toggle="modal" href="#delete_<?php echo $owners->pet_owner_id; ?>" class="badge  badge-pill badge-danger"><em class="fas fa-trash"></em> Delete</a>
+                                                            </td>
+                                                        </tr>
+                                                    <?php
+                                                        /* Manage Admin Modals */
+                                                        include('../app/modals/pet_owners.php');
+                                                    } ?>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                    <!-- /.row -->
+                                </div>
+
+                            </div>
+                            <!-- /.card -->
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                    <!-- /.row -->
+                </div>
+                <!--/. container-fluid -->
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+
+        <!-- Main Footer -->
+        <?php require_once('../app/partials/footer.php'); ?>
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- REQUIRED SCRIPTS -->
+    <?php require_once('../app/partials/scripts.php'); ?>
+</body>
