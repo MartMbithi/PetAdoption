57594b2752e7be410e426f863943a6b8badec02e
Martin Mbithi <martdevelopers254@gmail.com>
2022-07-30 11:52:49 +0300

    Implement Pet Adoption Modal

--- app/modals/pets.php
+++ app/modals/pets.php
@@ -1,3 +1,54 @@
+<!-- Adopt Pet -->
+<div class="modal fade" id="adopt_<?php echo $pets->pet_id; ?>">
+    <div class="modal-dialog modal-dialog-centered  modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Adopt <?php echo $pets->pet_name; ?></h4>
+                <button type="button" class="close" data-dismiss="modal">
+                    <span>&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form method="POST">
+                    <div class="form-row">
+                        <div class="form-group col-8">
+                            <label>Select Pet Adopter</label>
+                            <div class="input-group mb-3">
+                                <select class="form-control" required type="text" name="pet_adoption_adopter_id">
+                                    <option>Select New Pet Owner</option>
+                                    <?php
+                                    $adopter_ret = "SELECT * FROM  adopter";
+                                    $adopter_stmt = $mysqli->prepare($adopter_ret);
+                                    $adopter_stmt->execute(); //ok
+                                    $adopter_res = $adopter_stmt->get_result();
+                                    while ($adopters = $adopter_res->fetch_object()) {
+                                    ?>
+                                        <option><?php echo $adopters->adopter_full_name; ?></option>
+                                    <?php
+                                    } ?>
+                                </select>
+                                <input class="form-control" value="<?php echo $pets->pet_id; ?>" required type="hidden" name="pet_adoption_pet_id">
+                                <input class="form-control" value="<?php echo $pets->pet_id; ?>" required type="hidden" name="pet_adoption_pet_id">
+                                <div class="input-group-append">
+                                    <div class="input-group-text">
+                                        <span class="text-primary fas fa-user-md"></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group text-right">
+                        <button class="btn btn-outline-primary mt-3" type="submit" name="Adopt_Pet" name="submit">
+                            <i class="fas fa-cat"></i> Save
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 <!-- Modals -->
 <div class="modal fade" id="update_<?php echo $pets->pet_id; ?>">
     <div class="modal-dialog modal-dialog-centered  modal-lg">
