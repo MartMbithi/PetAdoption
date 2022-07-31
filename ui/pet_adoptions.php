fe59fca81ea8347ec90efb652592ba0efa589d54
Martin Mbithi <martdevelopers254@gmail.com>
2022-07-31 09:22:13 +0300

    Implement Pet Adoptions - Fetch All Adoptions - With Manage Buttons

--- ui/pet_adoptions.php
+++ ui/pet_adoptions.php
@@ -101,109 +101,10 @@ require_once('../app/partials/head.php');
                         <hr>
                     </div><!-- /.row -->
                 </div><!-- /.container-fluid -->
-                <div class="card-header p-2">
-                    <h3 class="text-right">
-                        <button type="button" data-toggle="modal" data-target="#add_modal" class="btn btn-outline-primary">
-                            <i class="fas fa-cat"></i> Register New Pet
-                        </button>
-                    </h3>
-                </div><!-- /.card-header -->
             </div>
             <!-- /.content-header -->
 
-            <div class="modal fade" id="add_modal">
-                <div class="modal-dialog modal-dialog-centered  modal-lg">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h4 class="modal-title">Register New Pet - Fill All Required Fields </h4>
-                            <button type="button" class="close" data-dismiss="modal">
-                                <span>&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <form method="POST">
-                                <div class="form-row">
-                                    <div class="form-group col-8">
-                                        <label>Name</label>
-                                        <div class="input-group mb-3">
-                                            <input class="form-control" required type="text" name="pet_name">
-                                            <div class="input-group-append">
-                                                <div class="input-group-text">
-                                                    <span class="text-primary fas fa-paw"></span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group col-4">
-                                        <label>Age</label>
-                                        <div class="input-group mb-3">
-                                            <input class="form-control" required type="text" name="pet_age">
-                                            <div class="input-group-append">
-                                                <div class="input-group-text">
-                                                    <span class="text-primary fas fa-calendar"></span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group col-6">
-                                        <label>Breed</label>
-                                        <div class="input-group mb-3">
-                                            <input class="form-control" required type="text" name="pet_breed">
-                                            <div class="input-group-append">
-                                                <div class="input-group-text">
-                                                    <span class="text-primary fas fa-venus-mars"></span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group col-6">
-                                        <label>Health Status</label>
-                                        <div class="input-group mb-3">
-                                            <select class="form-control" required type="text" name="pet_health_status">
-                                                <option>Healthy</option>
-                                                <option>Ill</option>
-                                            </select>
-                                            <div class="input-group-append">
-                                                <div class="input-group-text">
-                                                    <span class="text-primary fas fa-capsules"></span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group col-12">
-                                        <label>Pet Owner</label>
-                                        <div class="input-group mb-3">
-                                            <select class="form-control" required type="text" name="pet_pet_owner">
-                                                <?php
-                                                $ret = "SELECT * FROM pet_owner";
-                                                $stmt = $mysqli->prepare($ret);
-                                                $stmt->execute(); //ok
-                                                $res = $stmt->get_result();
-                                                while ($owners = $res->fetch_object()) {
-                                                ?>
-                                                    <option value="<?php echo $owners->pet_owner_id; ?>"><?php echo $owners->pet_owner_full_name; ?></option>
-                                                <?php
-                                                } ?>
-                                            </select>
-                                            <div class="input-group-append">
-                                                <div class="input-group-text">
-                                                    <span class="text-primary fas fa-user-tag"></span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
 
-                                </div>
-                                <div class="form-group text-right">
-                                    <button class="btn btn-outline-primary mt-3" type="submit" name="Add_Pet" name="submit">
-                                        <i class="fas fa-cat"></i> Save
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
             <!-- Main content -->
             <section class="content">
@@ -221,6 +122,7 @@ require_once('../app/partials/head.php');
                                                         <th>Pet Owner Details</th>
                                                         <th>Adopted By</th>
                                                         <th>Date Adopted</th>
+                                                        <th>Manage</th>
                                                     </tr>
                                                 </thead>
                                                 <tbody>
@@ -252,6 +154,10 @@ require_once('../app/partials/head.php');
                                                             </td>
                                                             <td>
                                                                 <?php echo date('d M Y', strtotime($adoption->pet_adoption_date_adopted)); ?>
+                                                            </td>
+                                                            <td>
+                                                                <a data-toggle="modal" href="#update_<?php echo $adoption->pet_adoption_id; ?>" class="badge  badge-pill badge-warning"><em class="fas fa-user-edit"></em> Edit</a>
+                                                                <a data-toggle="modal" href="#delete_<?php echo $adoption->pet_adoption_id; ?>" class="badge  badge-pill badge-danger"><em class="fas fa-trash"></em> Delete</a>
                                                             </td>
                                                         </tr>
                                                     <?php
