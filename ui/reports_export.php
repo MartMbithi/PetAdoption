251a6e04fc1e07f5c3bc748648c65b88d847d39b
Martin Mbithi <martdevelopers254@gmail.com>
2022-08-04 18:30:10 +0300

    Successfully Patched Reports  - PDF Generator

--- ui/reports_export.php
+++ ui/reports_export.php
@@ -79,10 +79,12 @@ $dompdf = new Dompdf();
 $type = mysqli_real_escape_string($mysqli, $_GET['type']);
 $report = mysqli_real_escape_string($mysqli, $_GET['report']);
 
+/* Dates */
+$start = mysqli_real_escape_string($mysqli, $_GET['start']);
+$end = mysqli_real_escape_string($mysqli, $_GET['end']);
+
 /* 1. Pet Adoptions */
 if ($report == 'adoptions') {
-    $start = mysqli_real_escape_string($mysqli, $_GET['start']);
-    $end = mysqli_real_escape_string($mysqli, $_GET['end']);
 
     /* Stripped Dates */
     $formal_start_date = date('d M Y', strtotime($start));
@@ -138,6 +140,80 @@ if ($report == 'adoptions') {
     } else if ($type == 'PDF') {
         /* Dump To PDF */
         $html = '
+            <style type="text/css">
+                table {
+                    font-size: 12px;
+                    padding: 4px;
+                }
+
+                tr {
+                    page-break-after: always;
+                }
+
+                th {
+                    text-align: left;
+                    padding: 4pt;
+                }
+
+                td {
+                    padding: 5pt;
+                }
+
+                #b_border {
+                    border-bottom: dashed thin;
+                }
+
+                legend {
+                    color: #0b77b7;
+                    font-size: 1.2em;
+                }
+
+                #error_msg {
+                    text-align: left;
+                    font-size: 11px;
+                    color: red;
+                }
+
+                .header {
+                    margin-bottom: 20px;
+                    width: 100%;
+                    text-align: left;
+                    position: absolute;
+                    top: 0px;
+                }
+
+                .footer {
+                    width: 100%;
+                    text-align: center;
+                    position: fixed;
+                    bottom: 5px;
+                }
+
+                #no_border_table {
+                    border: none;
+                }
+
+                #bold_row {
+                    font-weight: bold;
+                }
+
+                #amount {
+                    text-align: right;
+                    font-weight: bold;
+                }
+
+                .pagenum:before {
+                    content: counter(page);
+                }
+
+                /* Thick red border */
+                hr.red {
+                    border: 1px solid red;
+                }
+                .list_header{
+                    font-family: "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif;
+                }
+            </style>
             <div class="list_header" align="center">
                 <h3>
                     Pet Adoption System
@@ -165,24 +241,25 @@ if ($report == 'adoptions') {
         $stmt = $mysqli->prepare($ret);
         $stmt->execute(); //ok
         $res = $stmt->get_result();
+        $cnt = 1;
         while ($adoption = $res->fetch_object()) {
             $html .=
                 '
                         <tr>
                             <td>' . $cnt . '</td>
                             <td>
-                                ' . $adoption->pet_name . ' <br>
-                                Breed :' . $adoption->pet_breed . ' <br>
+                               <b>' . $adoption->pet_name . '</b> <br>
+                                Breed:' . $adoption->pet_breed . ' <br>
                                 Age: ' . $adoption->pet_age . '
                             </td>
                             <td>
-                                ' . $adoption->pet_owner_full_name . ' <br>
-                                Email :' . $adoption->pet_owner_email . ' <br>
+                                <b>' . $adoption->pet_owner_full_name . '</b> <br>
+                                Email:' . $adoption->pet_owner_email . ' <br>
                                 Contacts: ' . $adoption->pet_owner_contacts . '
                             </td>
                             <td>
-                                ' . $adoption->adopter_full_name . ' <br>
-                                Email :' . $adoption->adopter_email . ' <br>
+                                <b>' . $adoption->adopter_full_name . '</b> <br>
+                                Email: ' . $adoption->adopter_email . ' <br>
                                 Contacts: ' . $adoption->adoper_contacts . '
                             </td>
                             <td>' . date('d M Y', strtotime($adoption->pet_adoption_date_adopted)) . '</td>
