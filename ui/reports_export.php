165e68bbec977322b6e5bbf4411770372d040400
Martin Mbithi <martdevelopers254@gmail.com>
2022-08-12 09:13:38 +0300

    Craft Pet Owners UI

--- ui/reports_export.php
+++ ui/reports_export.php
@@ -291,6 +291,184 @@ if ($report == '') {
 }
 
 /* 3. Pet Owners */
+if ($report == 'admins') {
+    /* Dump Reports To XLS File */
+    if ($type == 'excel') {
+        function filterData(&$str)
+        {
+            $str = preg_replace("/\t/", "\\t", $str);
+            $str = preg_replace("/\r?\n/", "\\n", $str);
+            if (strstr($str, '"')) $str = '"' . str_replace('"', '""', $str) . '"';
+        }
+
+        /* Excel File Name */
+        $fileName = "Administrators Reports.xls";
+
+        /* Excel Column Name */
+        $header = array("Administrators Reports");
+        $fields = array('Email Address', 'Access Level');
+
+
+        /* Implode Excel Data */
+        $excelDataHeader = implode("\t\t\t", array_values($header)) . "\n\n";
+        $excelData = implode("\t", array_values($fields)) . "\n";
+
+        /* Fetch All Records From The Database */
+        $query = $mysqli->query("SELECT * FROM login 
+        WHERE login_rank = 'Administrator' ");
+        if ($query->num_rows > 0) {
+            /* Load All Fetched Rows */
+            while ($row = $query->fetch_assoc()) {
+                $lineData = array($row['login_email'], $row['login_rank']);
+                array_walk($lineData, 'filterData');
+                $excelData .= implode("\t", array_values($lineData)) . "\n";
+            }
+        } else {
+            $excelData .= 'No Administrators Records Available...' . "\n";
+        }
+
+        /* Generate Header File Encordings For Download */
+        header("Content-Type: application/vnd.ms-excel");
+        header("Content-Disposition: attachment; filename=\"$fileName\"");
+
+        /* Render  Excel Data For Download */
+        echo $excelDataHeader;
+        echo $excelData;
+
+        exit;
+    } else if ($type == 'PDF') {
+        /* Dump To PDF */
+        $html = '
+            <style type="text/css">
+                table {
+                    font-size: 12px;
+                    padding: 4px;
+                }
+
+                tr {
+                    page-break-after: always;
+                }
+
+                th {
+                    text-align: left;
+                    padding: 4pt;
+                }
+
+                td {
+                    padding: 5pt;
+                }
+
+                #b_border {
+                    border-bottom: dashed thin;
+                }
+
+                legend {
+                    color: #0b77b7;
+                    font-size: 1.2em;
+                }
+
+                #error_msg {
+                    text-align: left;
+                    font-size: 11px;
+                    color: red;
+                }
+
+                .header {
+                    margin-bottom: 20px;
+                    width: 100%;
+                    text-align: left;
+                    position: absolute;
+                    top: 0px;
+                }
+
+                .footer {
+                    width: 100%;
+                    text-align: center;
+                    position: fixed;
+                    bottom: 5px;
+                }
+
+                #no_border_table {
+                    border: none;
+                }
+
+                #bold_row {
+                    font-weight: bold;
+                }
+
+                #amount {
+                    text-align: right;
+                    font-weight: bold;
+                }
+
+                .pagenum:before {
+                    content: counter(page);
+                }
+
+                /* Thick red border */
+                hr.red {
+                    border: 1px solid red;
+                }
+                .list_header{
+                    font-family: "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif;
+                }
+            </style>
+            <div class="list_header" align="center">
+                <h3>
+                    Pet Adoption System
+                </h3>
+                <hr style="width:100%" , color=black>
+                <h5>Administrators Reports</h5>
+            </div>
+            <table border="1" cellspacing="0" width="98%" style="font-size:9pt">
+                <thead>
+                    <tr>
+                        <th style="width:10%">#</th>
+                        <th style="width:100%">Email</th>
+                        <th style="width:40%">Login Rank</th>
+                    </tr>
+                </thead>
+                <tbody>
+                ';
+        $ret = "SELECT * FROM login 
+        WHERE login_rank = 'Administrator'";
+        $stmt = $mysqli->prepare($ret);
+        $stmt->execute(); //ok
+        $res = $stmt->get_result();
+        $cnt = 1;
+        while ($admin = $res->fetch_object()) {
+            $html .=
+                '
+                        <tr>
+                            <td>' . $cnt . '</td>
+                            <td>
+                               ' . $admin->login_email . '
+                            </td>
+                            <td>
+                            ' . $admin->login_rank . '
+                            </td>
+                        </tr>
+                    ';
+            $cnt = $cnt + 1;
+        }
+        $html .= '
+                </tbody>
+            </table>
+        ';
+        $dompdf->load_html($html);
+        $dompdf->set_paper('A4');
+        $dompdf->set_option('isHtml5ParserEnabled', true);
+        $dompdf->render();
+        $dompdf->stream('Administrators Reports', array("Attachment" => 1));
+        $options = $dompdf->getOptions();
+        $options->setDefaultFont('');
+        $dompdf->setOptions($options);
+    } else {
+        $_SESSION['error'] = 'Specify Report Type';
+        header('Location: admins');
+        exit;
+    }
+}
 
 /* 4. Pet Adopters */
 
